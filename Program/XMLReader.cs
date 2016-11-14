9255d72d6832a60ef1ae7ad7a8a5f20811aec41a
NAPIER-MAIL\08009004 <08009004>
2016-11-14 16:49:20 +0000

    started hacking XMLReader.cs & XMLWriter.cs

---
+++ Program/XMLReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Program
+{
+    class XMLReader
+    {
+        private static Object readXML()
+        {
+            XmlSerializer s = new XmlSerializer(typeof(T));
+            FileStream fs = new FileStream(@"holiday_camp.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+        }
+    }
+}
---
+++ Program/XMLWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Program
+{
+    /*
+     * 
+     * http://stackoverflow.com/questions/4123590/serialize-an-object-to-xml
+     */
+    class XMLWriter
+    {
+        public void AlgorithmInterface<T>(T obj)
+        {
+            try
+            {
+      //          writeCVSFile(data.FlightCode, generateCSVData(data));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred:" + e.Message);
+            }
+        }
+        
+        private String generateXmlData<T>(T value)
+        {
+            if (value == null) return string.Empty;  // Short circuit method if null object was passed
+            
+            StringBuilder xmlData = new StringBuilder();
+            XmlSerializer s = new XmlSerializer(typeof(T));
+
+            TextWriter WriteFileStream = new StreamWriter(@"h:\test.xml");
+            s.Serialize(WriteFileStream, value);
+
+            WriteFileStream.Close(); // Cleanup
+            
+            return xmlData.ToString();
+        }
+
+        private void writeXmlFile(String code, String xml)
+        {
+            System.IO.File.AppendAllText(code + ".xml", xml);
+        }
+    }
+}
