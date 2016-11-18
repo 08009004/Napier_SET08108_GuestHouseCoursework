17a6f9568158e9dd0d3d9d275c5a54cd228f5289
Ruiz <08009004@live.napier.ac.uk>
2016-11-18 12:01:46 +0000

    recovered XmlReader/Writer, added XmlWritable.cs

---
+++ Program/XmlWritable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Custom imports:
+using System.Xml.Serialization;
+
+namespace Program
+{
+    [Serializable]
+    abstract class XmlWritable
+    {
+        public XmlWritable() { }
+
+        public abstract String ToCsv();
+
+    }
+}
---
+++ Program/XmlWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Custom imports:
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Program
+{
+    class XmlWriter
+    {
+
+        public static void Persist<T>(T obj)
+        {
+   //         try
+   //         {
+                writeData(@"holiday_reservation_system.xml", obj);
+   //         }
+   //         catch (Exception e)
+   //         {
+   //             Console.WriteLine("An error occurred:" + e.Message);
+   //         }
+        }
+
+
+        private static void writeData<T>(String filepath, T obj)
+        {
+            StringBuilder xmlData = new StringBuilder();
+            XmlSerializer s = new XmlSerializer(typeof(T));
+
+            Console.WriteLine(filepath);
+            Console.Read();
+            TextWriter tw = new StreamWriter(filepath);
+            s.Serialize(tw, obj);
+            tw.Close(); // Cleanup
+        }
+
+    }
+}
