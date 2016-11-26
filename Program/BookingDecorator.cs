ed0dd19ccdc1ec073f57e4a4a1f0ef9376d16f10
Ruiz <08009004@live.napier.ac.uk>
2016-11-26 17:46:37 +0000

    hacked BookingComponent.cs and BookingDecorator.cs

---
+++ Program/BookingDecorator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    abstract class BookingDecorator : BookingComponent
+    {
+        // Property : the decorator component
+        private BookingComponent booking;
+
+        /*
+         * Sets the component that this decorator decorates.
+         */
+        public void SetComponent(BookingComponent component)
+        {
+            this.booking = component;
+        }
+
+        /*
+         * Returns a textual representation of the BookingDecorator 
+         * in order to persist it to a CSV file.
+         */
+        public override String ToCSV()
+        {
+            String csv = String.Empty;
+
+            if (booking != null)
+            {
+                csv = booking.ToCSV();
+            }
+
+            return csv;
+        }
+    }
+}
