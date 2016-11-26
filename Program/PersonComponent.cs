64da15575d07feabd17a670744ccb248d8d47438
Ruiz <08009004@live.napier.ac.uk>
2016-11-26 19:05:26 +0000

    documented PersonComponent.cs

--- Program/PersonComponent.cs
+++ Program/PersonComponent.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Program
 {
+    /*
+    * Abstract component class used to make sure that 
+    * components and decorators share the same specification
+    */
     abstract class PersonComponent
     {
+        /*
+         * Must return a textual representation of the  
+         * BookingComponent in order to persist it to a CSV file.
+         */
         public abstract String ToCSV();
     }
 }
