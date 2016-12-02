c77fd05be97403f74785d2dcc0b70f9d548ff9e1
Ruiz <08009004@live.napier.ac.uk>
2016-12-02 11:37:56 +0000

    CSVWriter   > now overwrites file if a booking with same ref number had previously been persisted   > now returns true if file was persisted succesfully or false if not

--- Program/CSVWriter.cs
+++ Program/CSVWriter.cs
@@ -18,17 +18,33 @@ namespace Program
         private static String dataDirectory = @"data";
 
         /*
-         * Persists a BookingComponent to 
-         * {dataDirectory}/{BOOKING_NUMBER}.csv
+         * Persists a BookingComponent to {dataDirectory}/{BOOKING_NUMBER}.csv 
+         * Returns true if data was persisted successfuly or false if not.
          */
         public static bool Persist(BookingComponent booking)
         {
+            String filePath = (String.Format(@"{0}/{1}.csv", dataDirectory, booking.GetBookingNb()));
+
             if (!Directory.Exists(dataDirectory))
             {
                 Directory.CreateDirectory(dataDirectory);
             }
-            System.IO.File.AppendAllText(String.Format(@"{0}/{1}.csv", dataDirectory, booking.GetBookingNb()), booking.ToCSV());
-            return false;
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            try
+            {
+                System.IO.File.AppendAllText(filePath, booking.ToCSV());
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
