ce20b6cfa96fb26793cdefdc5960a6a1323c8acd
Ruiz <08009004@live.napier.ac.uk>
2016-12-06 10:18:49 +0000

    Created ModelFacade.cs

---
+++ Program/ModelFacade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class ModelFacade
+    {
+        // PROPERTIES:
+
+        // points to the BookingFactory instance:
+        private BookingFactory bFact;
+
+        // ponts to the PersonFactory instance:
+        private PersonFactory pFact;
+
+        // points to a DataPersistenceFacade object:
+        private DataPersistenceFacade dpFacade;
+
+        // METHODS:
+
+        /*
+         * Constructor.
+         */
+        public ModelFacade()
+        {
+            bFact = BookingFactory.Instance;
+            pFact = PersonFactory.Instance;
+            dpFacade = new DataPersistenceFacade();
+        }
+    }
+}
