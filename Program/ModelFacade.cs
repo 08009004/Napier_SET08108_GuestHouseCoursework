97ecfd4fd8d4ef4ea404ceceae1e3cc1d4bf97cb
Ruiz <08009004@live.napier.ac.uk>
2016-12-06 12:32:07 +0000

    added header comment

--- Program/ModelFacade.cs
+++ Program/ModelFacade.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace Program
 {
+    /*
+     * Facade providing an interface to the system.
+     * 
+     * author: Pierre Ruiz (matriculation number 08009004)
+     * last modified: 2016-12-06
+     */
     public class ModelFacade
     {
         // PROPERTIES:
