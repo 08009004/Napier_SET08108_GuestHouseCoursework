f117bc2f0fce5434a970c18418d83d88f0c7ff25
Ruiz <08009004@live.napier.ac.uk>
2016-12-06 18:25:40 +0000

    documented class

--- Program/Bookings.xaml.cs
+++ Program/Bookings.xaml.cs
@@ -14,19 +14,30 @@ using System.Windows.Shapes;
 
 namespace Program
 {
-    /// <summary>
-    /// Interaction logic for Bookings.xaml
-    /// </summary>
+    /*
+     * Interaction logic for Bookings.xaml
+     * 
+     * author: Pierre Ruiz (matriculation number 08009004)
+     * last modified: 2016-12-06
+     */
     public partial class Bookings : Window
     {
+        // Property:
+        // points to a ModelFacade instance.
         private ModelFacade mFacade;
 
+        /*
+         * Constructor.
+         */
         public Bookings(ModelFacade mFacade)
         {
             this.mFacade = mFacade;
             InitializeComponent();
         }
 
+        /*
+         * Closes the window.
+         */
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
